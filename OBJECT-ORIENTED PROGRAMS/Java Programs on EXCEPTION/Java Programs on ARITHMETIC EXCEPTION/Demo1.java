eaac9efc3fbd109de4474b891433bd6140052d1f
Ram Pratap Ranjan <rpranjan11@gmail.com>
2019-01-11 16:13:31 +0530

    Update Demo1.java

--- OBJECT-ORIENTED PROGRAMS/Java Programs on EXCEPTION/Java Programs on ARITHMETIC EXCEPTION/Demo1.java
+++ OBJECT-ORIENTED PROGRAMS/Java Programs on EXCEPTION/Java Programs on ARITHMETIC EXCEPTION/Demo1.java
@@ -1,25 +1,28 @@
 /* java application to handled System defined Exception i:e ArithmeticException by programmer */
+
 import java.util.Scanner;
 class Demo1
 {
-public static void main(String args[])
-{
-	Scanner s=new Scanner(System.in);
-	System.out.print("Enter value for Numerator	::");
-	int nr=s.nextInt();
-	System.out.print("Enter value for Denominator	::");
-	int dr=s.nextInt();
-	try{
-	int result=nr/dr;
-	System.out.println("Division Result="+result);
-	}//close of try block
-	catch(ArithmeticException x){
-	System.out.println(x);
-	System.out.println(x.toString());
-	System.out.println(x.getMessage());
-	x.printStackTrace();
-	}//close of catch block
-System.out.println("Continue execution after catch block");
-}//close of main
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.print("Enter value for Numerator	::");
+		int nr=s.nextInt();
+		System.out.print("Enter value for Denominator	::");
+		int dr=s.nextInt();
+		try
+		{
+			int result=nr/dr;
+			System.out.println("Division Result="+result);
+		}//close of try block
+		catch(ArithmeticException x)
+		{
+			System.out.println(x);
+			System.out.println(x.toString());
+			System.out.println(x.getMessage());
+			x.printStackTrace();
+		}//close of catch block
+		System.out.println("Continue execution after catch block");
+	}//close of main
 }//close of class
-//OUTPUT
+
